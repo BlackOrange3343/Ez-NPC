52ba34caed756d79de76cf639890e6abe19a9f55
Mystifine <35544772+BlackOrange3343@users.noreply.github.com>
2020-09-03 18:48:22 -0400

    Update Properties.lua

--- Properties.lua
+++ Properties.lua
@@ -20,31 +20,31 @@ local Properties = {
 	NPC_UPDATE_INTERVAL = 0.5,
 	SPAWNER_UPDATE_INTERVAL = 3,
 	
-	NPC_SPAWNER_DIRECTORY = Workspace.Spawners,  -- Where spawners are at
+	NPC_SPAWNER_DIRECTORY = Workspace.Visuals.Spawners,  -- Where spawners are at
 	NPC_STORAGE_DIRECTORY = ServerStorage.NPCS, -- Where Models are stored
 	NPC_DIRECTORY = Workspace.Entities, -- Where Active NPC's Are at
 	
-	CREATE_MISC_DATA = false, -- This is preferred on false bubt can be set to true for thos who wish to have it on true
+	CREATE_MISC_DATA = true, -- This is preferred on false bubt can be set to true for thos who wish to have it on true
 	NPC_COLLISION = false, -- This is preferred on false
 	PLAYER_COLLISION = false, -- This is preferred on false
 	
 	HUMANOID_STATES = { -- Put humanoid states you don't want in here
-		[Enum.HumanoidStateType.Climbing] = false,
-		[Enum.HumanoidStateType.Dead] = false,
-		[Enum.HumanoidStateType.FallingDown] = true,
-		[Enum.HumanoidStateType.Freefall] = true,
-		[Enum.HumanoidStateType.Flying] = false,
-		[Enum.HumanoidStateType.GettingUp] = true,
-		[Enum.HumanoidStateType.Jumping] = true,
-		[Enum.HumanoidStateType.Landed] = true,
-		[Enum.HumanoidStateType.Physics] = true,
-		[Enum.HumanoidStateType.PlatformStanding] = true,
-		[Enum.HumanoidStateType.Ragdoll] = false,
-		[Enum.HumanoidStateType.Running] = true,
-		[Enum.HumanoidStateType.RunningNoPhysics] = true,
-		[Enum.HumanoidStateType.Seated] = false,
-		[Enum.HumanoidStateType.StrafingNoPhysics] = true,
-		[Enum.HumanoidStateType.Swimming] = false,
+		--[Enum.HumanoidStateType.Climbing] = true,
+		--[Enum.HumanoidStateType.Dead] = true,
+		--[Enum.HumanoidStateType.FallingDown] = true,
+		--[Enum.HumanoidStateType.Freefall] = true,
+		--[Enum.HumanoidStateType.Flying] = true,
+		--[Enum.HumanoidStateType.GettingUp] = true,
+		--[Enum.HumanoidStateType.Jumping] = true,
+		--[Enum.HumanoidStateType.Landed] = true,
+		--[Enum.HumanoidStateType.Physics] = true,
+		--[Enum.HumanoidStateType.PlatformStanding] = true,
+		--[Enum.HumanoidStateType.Ragdoll] = true,
+		--[Enum.HumanoidStateType.Running] = true,
+		--[Enum.HumanoidStateType.RunningNoPhysics] = true,
+		--[Enum.HumanoidStateType.Seated] = true,
+		--[Enum.HumanoidStateType.StrafingNoPhysics] = true,
+		--[Enum.HumanoidStateType.Swimming] = true,
 	}
 	
 }
